ba855da4f9b51473cf97c7ea7136de6075432e65
Laura Celis <lauracelis@lauracelis.lan>
2022-01-25 12:00:43 -0500

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <svg viewBox="0 0 1439 324" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.27657e-05 288.597V-1H1439V131.044C1406.5 147 1332 161.414 1277.5 156.5C1124.42 142.698 945.987 122.43 830.866 116.039C623.65 104.535 629.656 197.067 510.032 256.086C390.407 315.106 0.000138988 357.12 4.27657e-05 288.597Z" fill="#0C1618"/>
+                <text x="10" y="10" fill="red">Kauai</text>
+            </svg>
+            
+        </div>
+        )
+    }
+export default Header;
