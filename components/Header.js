9c26850633dadb979b9465f7c3f0646ce50e9e34
Laura Celis <lauracelis@lauracelis.lan>
2022-02-01 15:32:14 -0500

    setting up header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
+import styles from '../styles/Header.module.scss';
+//https://www.youtube.com/watch?v=lPJVi797Uy0&ab_channel=Fireship
 const Header = () => {
     return (
-        <div className="header">
-            <svg viewBox="0 0 1439 324" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4.27657e-05 288.597V-1H1439V131.044C1406.5 147 1332 161.414 1277.5 156.5C1124.42 142.698 945.987 122.43 830.866 116.039C623.65 104.535 629.656 197.067 510.032 256.086C390.407 315.106 0.000138988 357.12 4.27657e-05 288.597Z" fill="#0C1618"/>
-                <text x="10" y="10" fill="red">Kauai</text>
-            </svg>
-            
+        <div>
+            <nav className={styles.nav}>
+                <a href='/'>
+                    <div>
+                        <h1 className={styles.title}>Courtney Woodward</h1>
+                        <h1 className={styles.specialHeader}>Illustrator</h1>
+                    </div>
+                </a>
+                <div>
+                    <h2><a href='/gallery'>Gallery</a></h2>
+                </div>
+            </nav>
         </div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
+import Header from '../components/Header';
+import styles from '../styles/Header.module.scss';
 
 export default function Home() {
   return (
-    <div className="home">
+    <div className={styles.container}>
       <Header />
     </div>
   )
